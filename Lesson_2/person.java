381c8940e77497420b8b055b0d50979bd82aacb1
rmikhay <r_mikhaydarov@mail.ru>
2023-02-26 19:06:19 +0500

    My project

---
+++ Lesson_2/person.java
@@ -0,0 +1,22 @@
+public class person {
+    String sex = "male";
+    String name = "Roman";
+    double heght = 1.84;
+    int weight = 94;
+    int age = 30;
+
+    void run() {
+    }
+
+    void sit() {
+    }
+
+    void go() {
+    }
+
+    void speak() {
+    }
+
+    void learnJava() {
+    }
+}
