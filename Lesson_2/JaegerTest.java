d4a37bec2567629e8f3a8e54c7739d37b8d3b7b3
rmikhay <r_mikhaydarov@mail.ru>
2023-03-11 19:11:59 +0500

    Выполнено

---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,19 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setModelName("Gipsy Danger");
+        jaeger1.setMark("Mark-3");
+        jaeger1.setHeight(260.0);
+        jaeger1.setWeight(1.98);
+        jaeger1.setWeight(-5);
+        jaeger1.setSpeed(7);
+        jaeger1.setArmor(6);
+        jaeger1.setStrength(8);
+        jaeger1.setPilotsNumber(3);
+        Jaeger jaeger2 = new Jaeger("Coyote Tango", "Mark-1", 280.0, 2.312, 5, 4, 7, 2);
+        jaeger1.move();
+        System.out.println("Модель " + jaeger2.getModelName() + " = " + jaeger2.getMark());
+        System.out.println("Скорость " + jaeger1.getModelName() + " = " + jaeger1.getSpeed());
+
+    }
+}
